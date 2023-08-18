a0c8d5fba36a02c621ece2b370c9327877460545
luis71913667 <1427092@senati.pe>
2023-08-18 15:47:59 -0500

    ingreso de datos

---
+++ operacionesv/src/operacionesv/Operacionesv.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operacionesv;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Operacionesv {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("INTRODUSCA EL PRIMER NUMERO: ");
+        Scanner valor1 = new Scanner(System.in);
+        int numero1=valor1.nextInt();
+        System.out.println("INTRODUSCA EL SEGUNDO NUMERO: ");
+        Scanner valor2 = new Scanner(System.in);
+        int numero2=valor2.nextInt();
+        
+        
+    }
+    
+}
